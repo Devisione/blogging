814bfc3023b5c49e41f2b78353b5874dbc9a2ddd
Владимир Линкевич <stipafk@gmail.com>
2025-06-14 22:34:11 +0300

    temp changes

--- apps/frontend/src/screens/Publication/model/store/useGenerateDescription.tsx
+++ apps/frontend/src/screens/Publication/model/store/useGenerateDescription.tsx
@@ -1,1 +1,8 @@
-export const useGenerateDescription = () => {};
+import { useCallback } from "react";
+
+export const useGenerateDescription = () => {
+  const generate = useCallback(() => {
+    console.log(123);
+  }, []);
+  return generate;
+};
--- apps/frontend/src/screens/Publication/model/store/useGenerateName.tsx
+++ apps/frontend/src/screens/Publication/model/store/useGenerateName.tsx
@@ -1,6 +1,8 @@
 import { useCallback } from "react";
 
 export const useGenerateName = () => {
-  const generate = useCallback(() => {}, []);
+  const generate = useCallback(() => {
+    console.log(123);
+  }, []);
   return generate;
 };
